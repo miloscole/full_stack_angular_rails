d3b2ad5dba0fca42fb78b7245ad9b0bdbe680c16
miloscole <27miloscole@gmail.com>
2025-01-12 11:27:49 +0100

    Add cors, add validations, add angular frontend

---
+++ client_app/src/app/features/cars/services/cars.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Car } from '../models/car.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CarsService {
+  private apiUrl = 'http://localhost:3000/api/cars';
+
+  constructor(private http: HttpClient) {}
+
+  getCars(): Observable<Car[]> {
+    return this.http.get<Car[]>(this.apiUrl);
+  }
+
+  getCar(id: number): Observable<Car> {
+    return this.http.get<Car>(`${this.apiUrl}/${id}`);
+  }
+
+  createCar(data: Partial<Car>): Observable<Car> {
+    return this.http.post<Car>(this.apiUrl, data);
+  }
+
+  updateCar(id: number, data: Partial<Car>): Observable<Car> {
+    return this.http.put<Car>(`${this.apiUrl}/${id}`, data);
+  }
+
+  deleteCar(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}

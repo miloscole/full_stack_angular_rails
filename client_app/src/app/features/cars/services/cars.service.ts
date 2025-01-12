4ca93c1cea7acd2a669b46d599c219e2903f1ff0
miloscole <27miloscole@gmail.com>
2025-01-12 14:07:57 +0100

    Add development environment

--- client_app/src/app/features/cars/services/cars.service.ts
+++ client_app/src/app/features/cars/services/cars.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Car } from '../models/car.model';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CarsService {
-  private apiUrl = 'http://localhost:3000/api/cars';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
